d05c3ff37f8d03b5c617b4816867a67271c40412
Roman <76904779+Mr-R0m4n@users.noreply.github.com>
2021-01-06 23:58:18 +0100

    correction

--- Main.java
+++ Main.java
@@ -6,8 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Bitte geben Sie die Spielfeld größe ein: ");
         Scanner scMenu = new Scanner(System.in);
         int size = scMenu.nextInt();
+
         Matchfield matchfield = new Matchfield(size);
 
         matchfield.createMatchfield();
--- Matchfield.java
+++ Matchfield.java
@@ -1,28 +1,29 @@
 package SchiffeVersenken;
 
-import java.util.Scanner;
 
 public class Matchfield{
-        Scanner scSize = new Scanner(System.in);
     //Attributes
         private int fields;
-        private char[][] matchField = new char[fields][fields];
+        private final char[][] matchField = new char[fields][fields];
     //Constructors
         public Matchfield(int fields){
             this.fields = fields;
     }
     //Methods
         public void createMatchfield(){
-            for (int i1 = 0; i1 < matchField.length; i1++){
-                for (int i2 = 0; i2 < matchField[i1].length; i2++){
-                    matchField[i1][i2] = '~';
+            for (int i1 = 0; i1 < this.matchField.length; i1++){
+                for (int i2 = 0; i2 < this.matchField[i1].length; i2++){
+                    this.matchField[i1][i2] = '~';
                 }
             }
         }
 
         public void printMatchfield(){
-            for(char[] field : matchField) {
-                System.out.println(field);
+            for (char[] fieldX : this.matchField) {
+                for (char fieldY : fieldX) {
+                    System.out.println(fieldY);
+                }
+                System.out.println();
             }
         }
 
