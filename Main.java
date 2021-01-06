a4cdb519695ddbbb19a19e4e968c186a4ea7e9ae
Roman <76904779+Mr-R0m4n@users.noreply.github.com>
2021-01-06 23:32:00 +0100

    Add Methods create and print Matchfield

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 package SchiffeVersenken;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scMenu = new Scanner(System.in);
+        int size = scMenu.nextInt();
+        Matchfield matchfield = new Matchfield(size);
 
+        matchfield.createMatchfield();
+        matchfield.printMatchfield();
 
     }
 }
--- Matchfield.java
+++ Matchfield.java
@@ -1,4 +1,32 @@
 package SchiffeVersenken;
 
-public class Matchfield {
+import java.util.Scanner;
+
+public class Matchfield{
+        Scanner scSize = new Scanner(System.in);
+    //Attributes
+        private int fields;
+        private char[][] matchField = new char[fields][fields];
+    //Constructors
+        public Matchfield(int fields){
+            this.fields = fields;
+    }
+    //Methods
+        public void createMatchfield(){
+            for (int i1 = 0; i1 < matchField.length; i1++){
+                for (int i2 = 0; i2 < matchField[i1].length; i2++){
+                    matchField[i1][i2] = '~';
+                }
+            }
+        }
+
+        public void printMatchfield(){
+            for(char[] field : matchField) {
+                System.out.println(field);
+            }
+        }
+
+    //Exceptions
+
+    //Getters&Setters
 }
