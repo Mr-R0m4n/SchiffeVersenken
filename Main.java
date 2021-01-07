fc0a1a092be17d03c060a1e5bca56f22c3bdd5a4
Roman <76904779+Mr-R0m4n@users.noreply.github.com>
2021-01-08 00:34:10 +0100

    extends print Matchfield, decrease player options

--- Main.java
+++ Main.java
@@ -14,10 +14,9 @@ public class Main {
             System.out.println("*****WILLKOMMEN ZU SCHIFFE VERSENKEN*****");
             System.out.println("-----------------------------------------");
             System.out.println();
-            System.out.println("1 = Anzahl der Spieler");
-            System.out.println("2 = Spiel konfigurieren");
-            System.out.println("3 = Spiel starten");
-            System.out.println("4 = Spiel beenden");
+            System.out.println("1 = Spiel konfigurieren");
+            System.out.println("2 = Spiel starten");
+            System.out.println("3 = Spiel beenden");
             System.out.println();
             System.out.println("-----------------------------------------");
 
@@ -25,18 +24,12 @@ public class Main {
             switch (scMenu.nextInt()) {
                 case 1 -> {
                     System.out.println();
-                    System.out.println("1 Spieler");
-                    System.out.println("2 Spieler");
-                    System.out.println();
-                }
-                case 2 -> {
-                    System.out.println();
-                    System.out.println("Bitte geben Sie eine größe (Ganzzahl) für das Spielfeld ein");
+                    System.out.println("Bitte geben Sie eine größe (1 - 9) für das Spielfeld ein: ");
                     //Spielfeldgröße festlegen
                     matchfield.setMatchFieldSize();
                     System.out.println();
                 }
-                case 3 -> {
+                case 2 -> {
                     System.out.println();
                     //Spielfeld erstellen
                     matchfield.createMatchfield();
@@ -44,7 +37,7 @@ public class Main {
                     matchfield.printMatchfield();
                     System.out.println();
                 }
-                case 4 -> {
+                case 3 -> {
                     System.out.println();
                     System.out.println("Bis zum nächsten mal!");
                     loopGame = false;
--- Matchfield.java
+++ Matchfield.java
@@ -1,22 +1,36 @@
 package SchiffeVersenken;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Matchfield{
     //Attributes
         private char[][] matchField;
+        private int[] matchfieldIndex;
     //Constructors
 
     //Methods
         public void setMatchFieldSize(){
             Scanner scSize = new Scanner(System.in);
             int size = scSize.nextInt();
+            if(size < 0 || size > 9){
+                System.out.println();
+                System.out.println("Ungültige Eingabe!");
+                System.out.println();
+            }
             char[][] Matchfieldsize = new char[size][size];
             this.matchField = Matchfieldsize;
+
+            int[] matchfieldIndex = new int[size];
+            this.matchfieldIndex = matchfieldIndex;
         }
 
         public void createMatchfield(){
-            //System.out.println("ABCDE");
+            //Matchfield index
+            for (int im = 0; im < this.matchfieldIndex.length; im++) {
+                this.matchfieldIndex[im] = im+1;
+            }
+            //Matchfield field
             for (int i = 0; i < this.matchField.length; i++) {
                 for (int j = 0; j < this.matchField[i].length; j++) {
                     this.matchField[i][j] = '~';
@@ -24,10 +38,34 @@ public class Matchfield{
             }
         }
 
+        public static String IntToChar(char[] a) {
+            if (a == null)
+                return "null";
+            int iMax = a.length - 1;
+            if (iMax == -1)
+                return "[]";
+
+            StringBuilder b = new StringBuilder();
+            b.append(' ');
+            for (int i = 0; ; i++) {
+                b.append(a[i]);
+                if (i == iMax)
+                    return b.append(' ').toString();
+                b.append(" ");
+            }
+        }
+
         public void printMatchfield(){
+            System.out.print(" ");
+            char[] letters = new char[this.matchfieldIndex.length];
+            for (int im = 0; im < this.matchfieldIndex.length; im++) {
+                letters[im] =  (char)('A' + im);
+            }
+            System.out.println(IntToChar(letters));
             for (int i = 0; i < this.matchField.length; i++) {
+                System.out.print(this.matchfieldIndex[i] + " ");
                 for (int j = 0; j < this.matchField[i].length; j++) {
-                    System.out.print(this.matchField[i][j]);
+                    System.out.print(this.matchField[i][j] + " ");
                 }
                 System.out.println();
             }
